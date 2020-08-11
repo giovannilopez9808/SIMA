7252dca77cd231172d86aba1418647ee12ffb215
giovannilopez <giovannilopez9808@gmail.com>
2020-08-11 17:42:09 -0500

    [ADD] MODIS

--- Scripts/AODGraphics.py
+++ Scripts/AODGraphics.py
@@ -1,19 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
-car="../Stations/noroeste/"
-dates=np.loadtxt(car+"DataAOD2.txt",dtype=str,usecols=0)
-aod=np.loadtxt(car+"DataAOD2.txt",usecols=5)
-n=np.size(dates)
-days=np.ones(n)
-for i in range(n):
-    year=int("20"+dates[i][0:2])
-    month=int(dates[i][2:4])
-    day=int(dates[i][4:6])
-    days[i]=(datetime.date(year,month,day)-datetime.date(2015,1,1)).days
-plt.ylim(0,1)
-plt.xticks(np.arange(0,365*5,365),np.arange(2015,2020,1))
-plt.title("Noroeste")
-plt.plot(days,aod)
-plt.plot([0,365*5],[0.8,0.8],color="red",ls="--")
-plt.show()
+stations=["noreste","noroeste"]
+for station in stations:
+    car="../Stations/"+station+"/"
+    dates=np.loadtxt(car+"DataAOD.txt",dtype=str,usecols=0)
+    aod=np.loadtxt(car+"DataAOD.txt",usecols=5)
+    n=np.size(dates)
+    days=np.ones(n)
+    for i in range(n):
+        year=int("20"+dates[i][0:2])
+        month=int(dates[i][2:4])
+        day=int(dates[i][4:6])
+        days[i]=(datetime.date(year,month,day)-datetime.date(2015,1,1)).days
+    plt.ylim(0,1)
+    plt.xticks(np.arange(0,365*5,365),np.arange(2015,2020,1))
+    plt.title(station)
+    plt.plot(days,aod)
+    plt.plot([0,365*5],[0.8,0.8],color="red",ls="--")
+    plt.savefig(car+"Graphics/AOD.png")
+    plt.clf()
--- Scripts/DaysGraphicsSMARTS.py
+++ Scripts/DaysGraphicsSMARTS.py
@@ -1,21 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+#<----------Libreria para ubicar los errores------------>
+import errno
 #<----------------------------Lectura de los datos de entrada--------------------------------------->
-car="../../Stations/"
+car="../Stations/"
 stations=["noreste","noroeste"]
 for station in stations:
+    print("Graficando estacion "+station)
     carp=car+station
-    dates=np.loadtxt(carp+"/DataAOD.txt",usecols=0,dtype=str)
-    os.mkdir(carp+"/Graphics")
-    for date in dates:
+    dates,aods=np.loadtxt(carp+"/DataAOD.txt",usecols=[0,5],dtype=str,unpack=True)
+    try: #Direccion y nombre de la carpeta
+        os.mkdir(carp+"/Graphics")
+    #Verificacion si la carpeta ya existe o no
+    except OSError as e: #Si no se produce error realizar nada
+        if e.errno!=errno.EEXIST:
+            raise
+    for date,aod in zip(dates,aods):
         med=np.loadtxt(carp+"/Mediciones/"+date+".txt")
         mod=np.loadtxt(carp+"/ResultsSMARTS/"+date+".txt")
         plt.plot(med[:,0],med[:,1],label="Measurement",lw=3)
         plt.plot(mod[:,0],mod[:,1],label="SMARTS Model",lw=3)
-        plt.ylabel("Irradianca VIS-NIR-MIR (W/m$^2$)")
+        plt.ylabel("UV-VIS-NIR irradiance (W/m$^2$)")
         plt.xlabel("Local hour (h)")
-        plt.title(date)
-        plt.legend(ncol=2,frameon=False)
+        plt.xlim(8,17)
+        plt.ylim(0,1200)
+        plt.title("Day "+date+"\n AOD: "+aod)
+        plt.legend(ncol=2,frameon=False,mode="expand")
         plt.savefig(carp+"/Graphics/"+date+".png")
         plt.clf()
