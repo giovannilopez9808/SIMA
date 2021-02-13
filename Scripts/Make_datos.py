1830ca64c4b15a3f65ba26248cfd6963f42f95ac
Giovanni <giovannilopez9808@gmail.com>
2021-02-13 17:45:07 -0600

    New dataset

--- Scripts/Make_datos.py
+++ Scripts/Make_datos.py
@@ -1,7 +1,7 @@
 import numpy as np
 import datetime
-o3data = np.loadtxt("../Archivos/OzonoMty.csv", skiprows=1,
-                    usecols=np.arange(1, 17), dtype=str, delimiter=",")
+o3data = np.loadtxt("../Archivos/Ozono_OMI.csv", skiprows=1,
+                    usecols=np.arange(1, 7), dtype=str, delimiter=",")
 stations = ["noroeste","noreste"]
 dir_stations = "../Stations/"
 for station in stations:
@@ -11,7 +11,7 @@ for station in stations:
     for date in dates:
         year, month, day = int("20"+date[0:2]), int(date[2:4]), int(date[4:6])
         day2 = (datetime.date(year, month, day)-datetime.date(year, 1, 1)).days
-        o3 = o3data[day2, year-2005]
+        o3 = o3data[day2, year-2015]
         file.write(date+" "+o3+" "+str(year) +
                    " "+str(month)+" "+str(day)+"\n")
     file.close()
