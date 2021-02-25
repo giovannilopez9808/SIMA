8a81bb33a64f8cb00bc1eda90b2a6da8ab7da615
Giovanni <giovannilopez9808@gmail.com>
2021-02-25 04:55:14 -0600

    Add hourly and dauly means of PM10 July 2018

--- Scripts/Graphics_PM_2018_daily.py
+++ Scripts/Graphics_PM_2018_daily.py
@@ -12,7 +12,7 @@ inputs = {
 stations = ["NOROESTE", "NORESTE"]
 stations_titles = ["Northwest", "Northeast"]
 colors = ["r", "b"]
-plt.figure(figsize=(18, 8))
+plt.figure(figsize=(10, 8))
 for station, title, color in zip(stations, stations_titles, colors):
     PM_data = SIMA_data(
         inputs["year initial"],
@@ -21,20 +21,20 @@ for station, title, color in zip(stations, stations_titles, colors):
         inputs["particle type"],
     )
     PM_data.read_data(inputs["path data"])
-    PM_data.cut_year("2018-07-01", "2018-08-01")
-    PM_2018 = PM_data.section
+    PM_data.cut_year("2018-06-30", "2018-08-01")
+    PM_2018 = PM_data.section.groupby(PM_data.section.index.day).mean()
     plt.plot(np.arange(len(PM_2018)), PM_2018,
-             label=title, color=color)
-ticks = np.linspace(0, 31, 5, dtype=int)
+             label=title, color=color, lw=2.5, marker="o", ls="--")
+ticks = np.linspace(0, 30, 6, dtype=int)
 dates_ticks = []
 for day in ticks:
     dates_ticks.append(str(consecutiveday2date(int(day)+181, 2018)))
 plt.grid(ls="--", color="#000", alpha=0.5, lw=2)
-plt.xlim(0, 744)
-plt.ylim(0, 350)
+plt.xlim(0, 30)
+plt.ylim(0, 200)
 plt.legend(frameon=False, ncol=2, mode="expand", fontsize=16)
-plt.subplots_adjust(left=0.07, bottom=0.193, right=0.95, top=0.912)
-plt.xticks(ticks*24, dates_ticks, rotation=30, fontsize=16)
+plt.subplots_adjust(left=0.1, bottom=0.193, right=0.93, top=0.912)
+plt.xticks(ticks, dates_ticks, rotation=30, fontsize=16)
 plt.xlabel("Dates", fontsize=16)
 plt.ylabel("PM$_{10}$ $(\mu g/m^3)$", fontsize=16)
-plt.savefig(inputs["path graphics"]+"PM10_July_2018.png", dpi=400)
+plt.savefig(inputs["path graphics"]+"PM10_July_2018_day.png", dpi=400)
--- Scripts/Graphics_PM_2018_hourly.py
+++ Scripts/Graphics_PM_2018_hourly.py
@@ -12,7 +12,7 @@ inputs = {
 stations = ["NOROESTE", "NORESTE"]
 stations_titles = ["Northwest", "Northeast"]
 colors = ["r", "b"]
-plt.figure(figsize=(18, 8))
+plt.figure(figsize=(8, 6))
 for station, title, color in zip(stations, stations_titles, colors):
     PM_data = SIMA_data(
         inputs["year initial"],
@@ -22,19 +22,15 @@ for station, title, color in zip(stations, stations_titles, colors):
     )
     PM_data.read_data(inputs["path data"])
     PM_data.cut_year("2018-07-01", "2018-08-01")
-    PM_2018 = PM_data.section
+    PM_2018 = PM_data.section.groupby(PM_data.section.index.hour).mean()
     plt.plot(np.arange(len(PM_2018)), PM_2018,
-             label=title, color=color)
-ticks = np.linspace(0, 31, 5, dtype=int)
-dates_ticks = []
-for day in ticks:
-    dates_ticks.append(str(consecutiveday2date(int(day)+181, 2018)))
+             label=title, color=color, lw=2.5, marker="o", ls="--")
 plt.grid(ls="--", color="#000", alpha=0.5, lw=2)
-plt.xlim(0, 744)
-plt.ylim(0, 350)
+plt.xlim(0, 24)
+plt.ylim(0, 200)
+plt.xticks(np.linspace(0, 24, 7))
 plt.legend(frameon=False, ncol=2, mode="expand", fontsize=16)
-plt.subplots_adjust(left=0.07, bottom=0.193, right=0.95, top=0.912)
-plt.xticks(ticks*24, dates_ticks, rotation=30, fontsize=16)
+plt.subplots_adjust(left=0.12, bottom=0.11, right=0.94, top=0.912)
 plt.xlabel("Dates", fontsize=16)
 plt.ylabel("PM$_{10}$ $(\mu g/m^3)$", fontsize=16)
-plt.savefig(inputs["path graphics"]+"PM10_July_2018.png", dpi=400)
+plt.savefig(inputs["path graphics"]+"PM10_July_2018_hour.png", dpi=400)
