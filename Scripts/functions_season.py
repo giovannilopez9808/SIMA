27f6da1c1099dc2be468c0cfc4429fc814dc1234
Giovanni <giovannilopez9808@gmail.com>
2021-03-12 05:26:33 -0600

    Correction in hourly_means

--- Scripts/functions_season.py
+++ Scripts/functions_season.py
@@ -6,9 +6,7 @@ class season_data:
     def __init__(self, data):
         self.seasons = ['summer', 'autumn', 'winter', 'spring']
         self.data = data
-
-    def calc_hourly_day_mean(self):
-        days = {
+        self.days = {
             0: "monday",
             1: "tuesday",
             2: "wednesday",
@@ -17,15 +15,20 @@ class season_data:
             5: "saturday",
             6: "sunday",
         }
-        days_names = [days[day] for day in days]
+
+    def calc_hourly_day_mean(self):
+        days_names = [self.days[day] for day in self.days]
         self.data = pd.DataFrame(columns=days_names)
-        for day in days:
-            self.data[days[day]] = self.data.loc[self.data.index.weekday == day]
+        for day in self.days:
+            self.data[self.days[day]
+                      ] = self.data.loc[self.data.index.weekday == day]
 
     def calc_hourly_season_mean(self):
-        self.obtain_season_data(self)
-        self.data_season = self.data_season.groupby(
-            self.data_season.index.hour).mean()
+        self.hourly_season_mean = pd.DataFrame(columns=self.seasons)
+        for season in self.seasons:
+            data = self.obtain_season_data(self, season)
+            self.hourly_season_mean[season] = self.data.groupby(
+                self.data.index.hour).mean()
 
     def calc_season_data(self):
         self.decision_season = pd.DataFrame(columns=self.seasons)
