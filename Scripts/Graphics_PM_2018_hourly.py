1c1539b541f382475a7311e58d3c62d1059d2399
Giovanni <giovannilopez9808@gmail.com>
2021-03-26 19:58:45 -0600

    Generalize for all the stations

--- Scripts/Graphics_PM_2018_hourly.py
+++ Scripts/Graphics_PM_2018_hourly.py
@@ -5,15 +5,31 @@ inputs = {
     "year initial": 2015,
     "year final": 2020,
     "path data": "../Archivos/",
-    "path graphics": "../Graphics/",
+    "path graphics": "../Graphics/PM10/",
+    "stations names file": "Stations_name",
     "path stations": "../Stations/",
     "particle type": "PM10",
 }
-stations = ["NOROESTE", "NORESTE"]
-stations_titles = ["Northwest", "Northeast"]
-colors = ["r", "b"]
-plt.figure(figsize=(8, 6))
-for station, title, color in zip(stations, stations_titles, colors):
+stations = pd.read_csv(
+    inputs["path data"]+inputs["stations names file"]+".csv")
+stations_len = stations["Nombre"].count()
+colors = ['lightcoral',
+          'coral',
+          'chocolate',
+          'orange',
+          'lightgreen',
+          'springgreen',
+          'paleturquoise',
+          'lightskyblue',
+          'lightsteelblue',
+          'mediumpurple',
+          'violet',
+          'orchid',
+          'palevioletred']
+plt.figure(figsize=(10, 8))
+for station_i, color in zip(range(stations_len), colors):
+    station = stations["Nombre"][station_i]
+    title = stations["Name"][station_i].capitalize()
     PM_data = SIMA_data(
         inputs["year initial"],
         inputs["year final"],
@@ -25,12 +41,13 @@ for station, title, color in zip(stations, stations_titles, colors):
     PM_2018 = PM_data.section.groupby(PM_data.section.index.hour).mean()
     plt.plot(np.arange(len(PM_2018)), PM_2018,
              label=title, color=color, lw=2.5, marker="o", ls="--")
-plt.grid(ls="--", color="#000", alpha=0.5, lw=2)
+plt.grid(ls="--", color="#000000", alpha=0.5, lw=2)
 plt.xlim(0, 24)
-plt.ylim(0, 200)
+plt.ylim(20, 140)
 plt.xticks(np.linspace(0, 24, 7))
-plt.legend(frameon=False, ncol=2, mode="expand", fontsize=16)
+plt.legend(frameon=False, ncol=7, mode="expand",
+           fontsize=10, bbox_to_anchor=(0, 1.09, 1, 0.02))
 plt.subplots_adjust(left=0.12, bottom=0.11, right=0.94, top=0.912)
-plt.xlabel("Dates", fontsize=16)
+plt.xlabel("Local Time (h)", fontsize=16)
 plt.ylabel("PM$_{10}$ $(\mu g/m^3)$", fontsize=16)
-plt.savefig(inputs["path graphics"]+"PM10_July_2018_hour.png", dpi=400)
+plt.savefig(inputs["path graphics"]+"July_2018_hour.png", dpi=400)
--- Scripts/Graphics_PM_AOD_OMI_2018.py
+++ Scripts/Graphics_PM_AOD_OMI_2018.py
@@ -7,11 +7,49 @@ inputs = {"year initial": 2015,
           "day initial": "2018-07-01",
           "day final": "2018-08-01",
           "path data": "../Archivos/",
-          "station": "NOROESTE",
-          "stations": ["NOROESTE", "NORESTE"],
+          "path graphics": "../Graphics/PM10/",
+          "path stations": "../Stations/",
+          "stations names file": "Stations_name",
           # AOD specification
           "wavelength": "500",
           "resolution": "1", }
+stations = pd.read_csv(
+    inputs["path data"]+inputs["stations names file"]+".csv")
+stations_len = stations["Nombre"].count()
+colors = ['lightcoral',
+          'coral',
+          'chocolate',
+          'orange',
+          'lightgreen',
+          'springgreen',
+          'paleturquoise',
+          'lightskyblue',
+          'lightsteelblue',
+          'mediumpurple',
+          'violet',
+          'orchid',
+          'palevioletred']
+fig, ax = plt.subplots(figsize=(10, 8))
+ax2 = ax.twinx()
+for station_i, color in zip(range(stations_len), colors):
+    station = stations["Nombre"][station_i]
+    title = stations["Name"][station_i].capitalize()
+    PM10 = SIMA_data(inputs["year initial"],
+                     inputs["year final"],
+                     station,
+                     "PM10",)
+    PM10.read_data(inputs["path data"])
+    PM10.cut_data_hour_year(inputs["day initial"],
+                            inputs["day final"])
+    PM10_july_2018 = PM10.section.groupby(
+        PM10.section.index.day).mean()
+    x = np.array(PM10_july_2018.index)-1
+    ax.plot(x, list(PM10_july_2018[station]),
+            label=title,
+            ls="--",
+            marker="o",
+            color=color
+            )
 AOD = AOD_OMI_data(inputs["year initial"],
                    inputs["year final"],
                    inputs["wavelength"],
@@ -19,38 +57,22 @@ AOD = AOD_OMI_data(inputs["year initial"],
 AOD.read_data(inputs["path data"])
 AOD.cut_data_year(inputs["day initial"],
                   inputs["day final"])
-PM10_noroeste = SIMA_data(inputs["year initial"],
-                          inputs["year final"],
-                          inputs["stations"][0],
-                          "PM10",)
-PM10_noroeste.read_data(inputs["path data"])
-PM10_noroeste.cut_data_hour_year(inputs["day initial"],
-                                 inputs["day final"])
-PM10_noreste = SIMA_data(inputs["year initial"],
-                         inputs["year final"],
-                         inputs["stations"][1],
-                         "PM10",)
-PM10_noreste.read_data(inputs["path data"])
-PM10_noreste.cut_data_hour_year(inputs["day initial"],
-                                inputs["day final"])
-PM10_july_2018_noroeste = PM10_noroeste.section.groupby(
-    PM10_noroeste.section.index.day).mean()
-PM10_july_2018_noreste = PM10_noreste.section.groupby(
-    PM10_noreste.section.index.day).mean()
 AOD_dates = [i for i in range(len(AOD.section.index))]
-fig, ax = plt.subplots()
-ax2 = ax.twinx()
+ax.set_xlim(0, 30)
 ax.set_ylim(0, 200)
 ax2.set_ylim(0, 1)
-ax.plot(PM10_july_2018_noreste.index,
-        PM10_july_2018_noreste, label="Northeast", ls="--", marker="o")
-ax.plot(PM10_july_2018_noroeste.index,
-        PM10_july_2018_noroeste, label="Northwest", ls="--", marker="o")
+ticks = np.linspace(0, 30, 5, dtype=int)
+dates_ticks = []
+for day in ticks:
+    dates_ticks.append(str(consecutiveday2date(int(day)+181, 2018)))
+ax.set_xticks(ticks)
+ax.set_xticklabels(dates_ticks, rotation=30)
 ax2.plot(AOD_dates, AOD.section, label="OMI",
          color="green", ls="--", marker="o")
+ax.grid(ls="--", alpha=0.5, color="#000000")
 lines, labels = fig.axes[0].get_legend_handles_labels()
 lines.append(fig.axes[1].get_legend_handles_labels()[0][0])
 labels.append(fig.axes[1].get_legend_handles_labels()[1][0])
 fig.legend(lines, labels, loc="upper center",
-           ncol=5, frameon=False, fontsize=12)
-plt.show()
+           ncol=7, frameon=False, fontsize=9)
+plt.savefig(inputs["path graphics"]+'July_2018.png')
